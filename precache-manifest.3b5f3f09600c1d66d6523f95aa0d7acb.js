cdd98fde8fad8bb9767dceca3c8f817f0206bd94
tienzlatan <39383420+tienzlatan@users.noreply.github.com>
2020-10-19 22:06:55 +0700

    Updates

---
+++ precache-manifest.3b5f3f09600c1d66d6523f95aa0d7acb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c45868c466dce425b03835173a7e821a",
+    "url": "/reactjs-tutorial-tictactoe-func/index.html"
+  },
+  {
+    "revision": "8ce6544eeb07b679c212",
+    "url": "/reactjs-tutorial-tictactoe-func/static/css/main.c8dc3b12.chunk.css"
+  },
+  {
+    "revision": "d4beeb07b80aeb6b3f4c",
+    "url": "/reactjs-tutorial-tictactoe-func/static/js/2.f47b7d6e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactjs-tutorial-tictactoe-func/static/js/2.f47b7d6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ce6544eeb07b679c212",
+    "url": "/reactjs-tutorial-tictactoe-func/static/js/main.432da74f.chunk.js"
+  },
+  {
+    "revision": "8da8f46176eec31ec671",
+    "url": "/reactjs-tutorial-tictactoe-func/static/js/runtime-main.aed95f03.js"
+  }
+]);
